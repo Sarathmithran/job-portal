121ca09207d4b287b6e11e8dad7155b73ef18000
Sarath TV <sarathmithran99@gmail.com>
2025-08-20 18:18:40 +0530

    created hero component

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full text-center">
+                <h1 className="text-[35px] sm:text-[50px] md:text-[60px] lg:text-[70px] font-figtree font-bold leading-[42px] sm:leading-[60px] md:leading-[72px] lg:leading-[84px] text-white mb-4 sm:mb-6">
+                  Find Your Dream Job Today!
+                </h1>
+                <p className="text-base sm:text-lg lg:text-[18px] font-figtree font-medium leading-5 sm:leading-6 lg:leading-[22px] text-[#ffffffcc] max-w-2xl">
+                  Connecting Talent with Opportunity: Your Gateway to Career Success
+                </p>
+              </div>
+  )
+}
+
+export default Hero
